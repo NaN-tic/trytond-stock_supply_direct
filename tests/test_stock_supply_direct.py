a6d66b27a80f46488d411cf59028fa3262c23da7
resteve <resteve@zikzakmedia.com>
2015-12-01 01:36:30 +0100

    Tests

--- tests/test_stock_supply_direct.py
+++ tests/test_stock_supply_direct.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
+# This file is part of the stock_supply_direct module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockSupplyDirectTestCase(unittest.TestCase):
+class StockSupplyDirectTestCase(ModuleTestCase):
     'Test Stock Supply Direct module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_supply_direct')
-
-    def test0005views(self):
-        'Test views'
-        test_view('stock_supply_direct')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_supply_direct'
 
 
 def suite():
